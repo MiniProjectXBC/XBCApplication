6895e0338beab44c05895cbffe1b41b95890c4c0
julius1kusuma <44283593+julius1kusuma@users.noreply.github.com>
2018-10-19 17:47:41 +0400

    login menu finish
    FlashScreen masuk ke Login Menu setelah login masuk ke Home Menu

--- app/src/main/java/xbc/miniproject/com/xbcapplication/SplashScreenActivity.java
+++ app/src/main/java/xbc/miniproject/com/xbcapplication/SplashScreenActivity.java
@@ -4,45 +4,46 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 import xbc.miniproject.com.xbcapplication.utility.Constanta;
+import xbc.miniproject.com.xbcapplication.utility.SessionManager;
 
 public class SplashScreenActivity extends Activity {
-    private Context context = this;
+    Context context = this;
+    TextView textVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        
+        textVersion = (TextView)findViewById(R.id.textVersion);
+        getTextVersion();
+
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
-        //rahmat
-
-//        TimerTask timerTask = new TimerTask() {
-//            @Override
-//            public void run() {
-//                //pindah ke scree berikutnya
-//                if (SessionManager.isRegister(context)) {
-//                    //bypass ke main menu
-//                    Intent intent = new Intent(context, /*MainMenuActivity*/. class);
-//                    startActivity(intent);
-//                } else {
-//                    Intent intent = new Intent(context, /*WelcomeActivity*/. class);
-//                    startActivity(intent);
-//                }
-                Intent intent = new Intent(context, HomeActivity.class);
-                startActivity(intent);
-
+                //pindah ke scree berikutnya
+                if (SessionManager.isRegister(context)) {
+                    //bypass ke main menu
+                    Intent intent = new Intent(context, HomeActivity. class);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(context, LoginActivity. class);
+                    startActivity(intent);
+                }
                 finish();
             }
         };
 
         Timer timer = new Timer();
         timer.schedule(timerTask, Constanta.SPLASH_DELAY_TIME);
+    }
+    public void getTextVersion(){
+        String ver = BuildConfig.VERSION_NAME;
+        textVersion.setText("Version : "+ver);
     }
 }
