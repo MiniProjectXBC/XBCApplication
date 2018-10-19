ec6749267224c3e124e403930385c33ccea6acad
julius1kusuma <44283593+julius1kusuma@users.noreply.github.com>
2018-10-19 15:30:40 +0400

    splash screen

--- app/src/main/java/xbc/miniproject/com/xbcapplication/SplashScreenActivity.java
+++ app/src/main/java/xbc/miniproject/com/xbcapplication/SplashScreenActivity.java
@@ -1,17 +1,41 @@
 package xbc.miniproject.com.xbcapplication;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import xbc.miniproject.com.xbcapplication.utility.Constanta;
+
 public class SplashScreenActivity extends Activity {
-    //Tes Github Yuhananda
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        //ini rahmat
-        //commit 2
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                //pindah ke scree berikutnya
+                if (SessionManager.isRegister(context)) {
+                    //bypass ke main menu
+                    Intent intent = new Intent(context, /*MainMenuActivity*/. class);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(context, /*WelcomeActivity*/. class);
+                    startActivity(intent);
+                }
+
+                finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(timerTask, Constanta.SPLASH_DELAY_TIME);
     }
 }
