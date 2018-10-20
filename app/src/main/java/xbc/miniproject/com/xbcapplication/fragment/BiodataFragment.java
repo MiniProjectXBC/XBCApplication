0b909f7c1d66162b29aa7131ee56080b752e3260
yuhanadit <44283376+yuhanadit@users.noreply.github.com>
2018-10-20 11:29:20 +0700

    Menambah sedikit di fragment biodata

--- app/src/main/java/xbc/miniproject/com/xbcapplication/fragment/BiodataFragment.java
+++ app/src/main/java/xbc/miniproject/com/xbcapplication/fragment/BiodataFragment.java
@@ -5,22 +5,49 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import xbc.miniproject.com.xbcapplication.R;
 
 public class BiodataFragment extends Fragment {
-    Context context = getActivity();
+    EditText biodataEditTextSearch;
+    Button biodataButtonInsert;
+    RecyclerView biodataRecyclerViewList;
 
     public BiodataFragment() {
+
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_biodata,container,false);
+
+        //Cara mendapatkan Context di Fragment dengan menggunakan getActivity() atau getContext()
+        //Toast.makeText(getContext(),"Test Context Behasil", Toast.LENGTH_LONG).show();
+
+        biodataEditTextSearch = (EditText) view.findViewById(R.id.biodataEditTextSearch);
+        biodataButtonInsert = (Button) view.findViewById(R.id.biodataButtonInsert);
+        biodataButtonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        biodataRecyclerViewList = (RecyclerView) view.findViewById(R.id.biodataRecyclerViewList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(),
+                LinearLayout.VERTICAL, false);
+        biodataRecyclerViewList.setLayoutManager(layoutManager);
+
 
         return view;
     }
